6d7f68da48d2f0a5166e6a5f2fe820d364c5f9b8
Florian <imjaflo@gmail.com>
2020-10-26 19:34:30 -0500

    Update dependencies and download directly from yt-dl

--- deps.sh
+++ deps.sh
@@ -3,13 +3,9 @@
 cd /tmp
 rm -rf *
 
-curl -sL https://yt-dl.org/downloads/latest/youtube-dl -o youtube-dl
-curl -sL https://github.com/ytdl-org/youtube-dl/archive/master.zip -o youtube-dl-master.zip
+release=$(curl https://yt-dl.org | grep -Eo 'downloads[^\"]+gz')
+curl -sL https://yt-dl.org/$release -o youtube-dl.tar.gz
 
-chmod a+rx youtube-dl
-unzip -q youtube-dl-master.zip
-
-mv youtube-dl-master/youtube_dl ytdl
-rm -rf youtube-dl-master
-rm -rf youtube_dl
-mv ytdl youtube_dl
+tar -xf youtube-dl.tar.gz
+mv youtube-dl-*/youtube_dl youtube_dl
+rm -rf youtube-dl.tar.gz youtube-dl-*
